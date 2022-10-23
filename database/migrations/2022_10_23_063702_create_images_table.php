82aa7882c3dfd8fc0a0c4095543b7c880e2c5361
charles <charlesmuojekwu@gmail.com>
2022-10-23 07:44:19 +0100

    Added migration for the polymorhic relationship

--- database/migrations/2022_10_23_063702_create_images_table.php
+++ database/migrations/2022_10_23_063702_create_images_table.php
@@ -13,10 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('images', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->text('body');
+            $table->string('url');
+            $table->unsignedBigInteger('imageable_id');
+            $table->string('imageable_type');
             $table->timestamps();
         });
     }
@@ -28,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('images');
     }
 };
